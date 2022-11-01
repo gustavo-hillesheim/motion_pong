a57e339a326afa08fcf488479f2a52d1338c9632
Gustavo Hillesheim <guhillesheim@gmail.com>
2022-10-31 21:46:24 -0300

    Created keyboard bindings in PongTestPage

--- motion_pong/lib/pages/pong_test_page.dart
+++ motion_pong/lib/pages/pong_test_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pong/pong.dart';
 
 class PongTestPage extends StatelessWidget {
@@ -8,7 +9,31 @@ class PongTestPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Pong Test Page')),
-      body: const Pong(),
+      body: Shortcuts(
+        shortcuts: const {
+          SingleActivator(LogicalKeyboardKey.keyW):
+              MoveIntent(VerticalDirection.up),
+          SingleActivator(LogicalKeyboardKey.keyS):
+              MoveIntent(VerticalDirection.down),
+        },
+        child: Actions(
+          actions: {
+            MoveIntent: CallbackAction<MoveIntent>(
+              onInvoke: (intent) => debugPrint('moving ${intent.direction}'),
+            ),
+          },
+          child: const Focus(
+            autofocus: true,
+            child: Pong(),
+          ),
+        ),
+      ),
     );
   }
+}
+
+class MoveIntent extends Intent {
+  final VerticalDirection direction;
+
+  const MoveIntent(this.direction);
 }
